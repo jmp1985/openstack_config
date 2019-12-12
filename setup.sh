b629063773146d6273f659231e8b0fc7b097e25d
James Parkhurst <james.parkhurst@diamond.ac.uk>
2019-12-12 11:57:54 +0000

    Initial commit to add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Helper function to read the packages and strip any comments
+function _read_packages {
+    while read line
+    do
+        echo $line | cut -d"#" -f1
+    done < $1
+}
+
+set -e
+
+# Update the system
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install the required packages
+sudo apt install $(_read_packages packages.txt)
+
+# Remove unused packages
+sudo apt autoremove
